cda4b873a7eea3dcd36281a2b3d837c081441898
Armando Rojas <rojasarmando260@gmail.com>
2018-03-10 23:15:04 -0400

    Agregando la tabla usuarios en BD;

--- Base de Datos/BASE_DE_DATOS.sql
+++ Base de Datos/BASE_DE_DATOS.sql
@@ -1,15 +1,19 @@
-create database Mi_Cerenis_Cabrera
+create database Mi_Cerenis_Cabrera;
+
+use Mi_Cerenis_Cabrera;
 
 create table usuarios (
 
-	id int(10) NOT NULL AUTO_INCREMENT ,
-	nombre varchar(100),
+	id int unsigned auto_increment,
+	nombre varchar(100) not null unique,
 	clave varchar(100),
 	pregunta varchar(100),
 	respuesta varchar(100),
 	tipo_usuario int(2),
+	fecha_c datetime,
+	fecha_m datetime,
 	status TINYINT(1), 
-
-	primary key ('id')
+	
+	primary key (id)
 
 ); 
