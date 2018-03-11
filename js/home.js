1d88f505117e1bb554b6a1e5abc1219c11598c17
Armando Rojas <rojasarmando260@gmail.com>
2018-03-11 03:53:58 -0400

    Corrigiendo errores y agregando mi estilo

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-Menu();
+$(document).ready(function() {
+	
+Menu(); //invocamos los scripts del menu 
+
+	setInterval(function(){
+
+		$("#mi_reloj").html(miTiempo.hora_local())
+
+	}, 1000);	
+	
+
+
+});
+
+
--- vista/home.php
+++ vista/home.php
@@ -5,11 +5,34 @@
 		<div class="jumbotron text-center">
 			<h1 >
 				
-					Hola mani
+					Hola 
 
 
 			</h1>
+			<br>
+			<h3>
+				<span id=mi_reloj> </span>
+			</h3>
 		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 ">
+			
+			<div class="panel panel-info">
+				<div class="panel-header">
+						<div class="panel-title text-muted text-center">
+							<h2>Ed Sheeran - Kiss me </h2>
+						 </div>
+				 </div>
+
+				 <div class="panel-body text-center">
+				 <audio src="storage/cere.mp3" autoplay controls></audio>
+				 	
+				 </div>
+				
+			</div>
+
+		 </div>
 	</div>
 </div>
 
