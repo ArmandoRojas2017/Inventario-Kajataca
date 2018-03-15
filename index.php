682b77f5702b14861c35821c15ead3b47b8958a5
Armando Rojas <rojasarmando260@gmail.com>
2018-03-15 01:39:49 -0400

    Bug arreglado con cabezera y pie de pagina

--- index.php
+++ index.php
@@ -1,14 +1,43 @@
 <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 
-<?php 
+<?php  require_once 'include/Helpers.php' ?>
 
-	require_once 'include/Helpers.php'; 
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Kajataca 1.0</title>
+
+	<!-- Agregando Estilos  -->
+
+	<!-- Resetear la Pantalla -->
+	<link rel="stylesheet" href="vendor/normalize.css">
+	<!-- Agregar Bootstrap´mas el Tema -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+
+
+	
+	<link rel="stylesheet" href="css/ack.css">
+	<!-- Paquete de iconos -->
+	<link rel="stylesheet" href="vendor/icono/stylesheet.css">
+
+	<!-- Plugins -->
+
+	<!-- Para mostrar una descripcion -->
+	<link rel="stylesheet" href="vendor/jquery-flyout-master/jquery.flyout.css">
+	<!--  Wow Js -->
+	<link rel="stylesheet" href="vendor/wow/animate.css">
 	
 
+	<!-- fin de lo estilos -->
 
-	view('template/cabezera');
+		<!-- Icono  -->
+	<link rel="shortcut icon" href="images/icono.ico">
+</head>
 
-?>
+<body>
 
 
 
@@ -25,7 +54,31 @@ if( !controller($url) ) view('404');
 
 ?>
 	
-<?php 
-	view('template/pie', compact("url") );
+<script src="vendor/jquery/crazy.js" > </script>
+<script src="vendor/bootstrap/js/bootstrap.min.js" > </script>
+<script src="vendor/wow/wow.js" > </script>
 
-?>
+<script src="vendor/jquery-flyout-master/jquery.flyout.js" > </script>
+
+<script src="vendor/notify-bootstrap/bootstrap-notify.min.js" > </script>
+
+<script src="vendor/select/bootstrap-select.js" > </script>
+<script src="vendor/sweetAlert2/sweetalert2.js" > </script>
+
+<script src="js/jquery.kajataca.js" > </script>
+
+<script src="js/Menu.js" > </script>
+
+<!-- Script para cada Pagina  -->
+
+<?php if( file_exists("js/".$url.".js")  ): ?>
+	
+	<script src=<?= "'js/".$url.".js'" ?>  > </script>
+
+<?php endif ?>
+
+
+
+</body>
+</html>
+
