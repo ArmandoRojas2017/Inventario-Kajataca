8790980305471efbcca8be5e44431070fe7cd13b
Armando Rojas <rojasarmando260@gmail.com>
2018-03-15 03:10:28 -0400

    Mejorando layout table

--- vista/template/tabla.php
+++ vista/template/tabla.php
@@ -1,14 +1,25 @@
-<div class="row"> 
 
 
-	<table class="table table-responsive" >
+	<table class="table table-responsive " >
+
+		<thead class="bg-primary">
+			<tr>
+				<th>Hola</th>
+				<th>Hola</th>
+				
+				<th>Hola</th>
+			</tr>
+		</thead>
 		<tr>
 			<td>Hola</td>
 			<td> Hola</td>
+			<td> Hola</td>
+		
 		</tr>
 		<tr>
 			<td>Hola</td>
 			<td> Hola</td>
+			<td> Hola</td>
+			
 		</tr>
 	</table>
-</div>
--- vista/usuario.php
+++ vista/usuario.php
@@ -2,14 +2,17 @@
 <div class="separacion"></div>
 <div class="container"> 
 	<div class="row">
-		
 	
-
+			<div class="panel-footer">
+				<h3 class="text-center">Consultar Usuarios</h3>
+			</div>
+		
+		
+	</div>
+	<div class="row">
 			
 			<?php componentes('tabla') ?>
 		
-
-			
 		</div>
 	</div>
 </div>
