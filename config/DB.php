cdc1481401b894e50b2966ec73dcbeabae0adffd
Armando Rojas <rojasarmando260@gmail.com>
2018-03-19 22:34:45 -0400

    ajuste a la configuracion de la base de datos provicional

--- config/DB.php
+++ config/DB.php
@@ -1,9 +1,15 @@
 <?php 
 	
+	 const USUARIO_BD = "root";
+	 const CLAVE_BD = "12345678";
+	 const SERVIDOR_BD = "localhost";
+	 const BASE_DE_DATOS = "Mi_Cerenis_Cabrera";
+
 	 const USER = "root";
 	 const PASS = "12345678";
 	 const HOST = "localhost";
 	 const DATABASE = "Mi_Cerenis_Cabrera";
+
 
 
 	 const ENCONTRADO = 1;
