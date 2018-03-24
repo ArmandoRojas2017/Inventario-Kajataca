417c855576a5916c01a967eb6fd824cbbf88630c
Armando Rojas <rojasarmando260@gmail.com>
2018-03-24 03:12:13 -0400

    reporte de usuario creado

---
+++ ajax/Usuarios/reporte.php
@@ -0,0 +1,34 @@
+<?php 
+
+require('../../modelo/ReportesFPDF.php');
+require('../../modelo/Usuario.php');
+
+$modelo = new Usuario();
+$modelo->preparar_consulta();
+$datos = [];
+
+$arreglo = $modelo->get_array();
+
+for ($i=0; $i < count($arreglo); $i++) { 
+	
+	$datos[$i][0] = $arreglo[$i]['id_usuarios'];
+	$datos[$i][1] = $arreglo[$i]['nombre'];
+	$datos[$i][2] = $arreglo[$i]['nick'];
+	$datos[$i][3] = ($arreglo[$i]['status'] == 1) ? "ACTIVO" : "DESACTIVADO";
+}
+
+
+// Instanciation of inherited class
+$pdf = new ReportesFPDF();
+$encabezado = array("Id","Nombre y Apellido","Nick","Estado");
+
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+$pdf->dimension(array("31","62","52","40"));
+$pdf->FancyTable($encabezado, $datos);
+
+$pdf->Output();
+
+
+ ?>
---
+++ modelo/ReportesFPDF.php
@@ -0,0 +1,98 @@
+<?php 
+
+/*
+
+	Generados de PDF dinamicos, utilizando libreria FPDF
+	clase hijo de FPDF.
+
+	by: Armando Jose Rojas Querales - 2018
+
+ */
+
+$ruta= "libs/fpdf/fpdf.php";
+
+if(file_exists($ruta)) 
+		require($ruta);
+elseif(file_exists("../".$ruta)) 
+		require("../".$ruta);
+elseif(file_exists("../../".$ruta)) 
+		require("../../".$ruta);
+else 
+	exit("BASE DE DATOS NO ENCONTRADA EN ESTE SERVIDOR: LLAMAR AL 0414-5235969 PARA MAS INFORMACION");
+
+class ReportesFPDF extends FPDF
+{
+
+	protected $dimension;
+
+
+// Page header
+function Header()
+{
+	// Logo
+	$this->Image('../../images/logo.jpeg',10,6,185);
+	// Arial bold 15
+	$this->SetFont('Arial','B',15);
+	// Move to the right
+	$this->Cell(80);
+	
+	/*
+	// Title
+	$this->Cell(30,10,'Title',1,0,'C');
+	*/
+	// Line break
+	$this->Ln(50);
+}
+
+// Page footer
+function Footer()
+{
+	// Position at 1.5 cm from bottom
+	$this->SetY(-15);
+	// Arial italic 8
+	$this->SetFont('Arial','I',8);
+	// Page number
+	$this->Cell(0,10,'Pagina '.$this->PageNo().' de {nb}',0,0,'C');
+}
+
+// Colored table
+function FancyTable($header, $data)
+{
+    // Colors, line width and bold font
+    $this->SetFillColor(0, 132, 255);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(0, 132, 255);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // Header
+    $w = $this->dimension;
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Color and font restoration
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Data
+    $fill = false;
+    foreach($data as $row)
+    {	
+    	 for( $i = 0; $i<count($row); $i++)
+        	$this->Cell($w[$i],6,$row[$i],'LR',0,'L',$fill);
+       
+        $this->Ln();
+        $fill = !$fill;
+    }
+    // Closing line
+    $this->Cell(array_sum($w),0,'','T');
+}
+
+	function dimension($arreglo){
+
+		$this->dimension = $arreglo;
+	}
+
+}
+
+
+ ?>
